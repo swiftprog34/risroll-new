c881ff0719afd6ea5646c86a2007d9bab17dc371
swiftprog <swift.prog34@gmail.com>
2024-02-07 16:31:20 +0300

    fix visual issues

--- resources/views/client/contacts.blade.php
+++ resources/views/client/contacts.blade.php
@@ -25,52 +25,52 @@
             <p>{!! $cityWithNested->contact_page_info !!}</p>
         </div>
         <div class="contacts_grid">
-            <div class="item">
-                <div class="worktime h5">
-                    <div class="worktime_item ">
-                        ПН
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item ">
-                        ВТ
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item ">
-                        СР
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item ">
-                        ЧТ
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item ">
-                        ПТ
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item  active ">
-                        СБ
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                    <div class="worktime_item ">
-                        ВС
-                        <hr>
-                        11:00 <br>
-                        23:00
-                    </div>
-                </div>
-            </div>
+{{--            <div class="item">--}}
+{{--                <div class="worktime h5">--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        ПН--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        ВТ--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        СР--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        ЧТ--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        ПТ--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item  active ">--}}
+{{--                        СБ--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                    <div class="worktime_item ">--}}
+{{--                        ВС--}}
+{{--                        <hr>--}}
+{{--                        11:00 <br>--}}
+{{--                        23:00--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
             <div class="item">
                 <img class="icon" src="client/images/color_icons/ic_map.png" alt=""/>
                 <h3><strong>{{$cityWithNested->city_name}}</strong><br> @foreach($cityWithNested->pickupPoints as $point)
@@ -84,6 +84,10 @@
             <a class="item" href="{{$cityWithNested->vk_link}}" target="_blank">
                 <img class="icon" src="client/images/color_icons/ic_vk.png" alt=""/>
                 <h3>Группа ВК</h3>
+            </a>
+            <a class="item" href="{{$cityWithNested->instagram_link}}" target="_blank">
+                <img class="icon" src="client/images/color_icons/ic_instagram.png" alt=""/>
+                <h3>Инстаграм</h3>
             </a>
         </div>
         <div class="container">
